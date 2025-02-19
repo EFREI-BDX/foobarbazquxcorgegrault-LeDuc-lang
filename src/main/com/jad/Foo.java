16ef19b539a8a09417eba0039b13b28f38f6ebb0
bouss <inivasim4@gmail.com>
2025-02-19 21:27:34 +0100

    i have done all tables

--- src/main/com/jad/Foo.java
+++ src/main/com/jad/Foo.java
@@ -1,4 +1,44 @@
 package com.jad;
 
+import java.util.ArrayList;
+
 public class Foo {
+
+    private String bar;
+    private ArrayList<Baz> bazs;
+    private String qux;
+    private String corge;
+    private ArrayList<Grault> graults;
+
+    public Foo (String bar) {
+        this.bar = bar;
+    }
+
+    public void addBaz (Baz baz) {}
+
+    public void addGrault () {}
+
+    public String getBar() {
+        return this.bar;
+    }
+
+    public ArrayList<Baz> getBazs() {
+        return this.bazs;
+    }
+
+    public String getQux() {
+        return this.qux;
+    }
+
+    public String getCorge() {
+        return this.corge;
+    }
+
+    public void setCorge(String corge) {
+        this.corge = corge;
+    }
+
+    public ArrayList<Grault> getGraults() {
+        return this.graults;
+    }
 }
